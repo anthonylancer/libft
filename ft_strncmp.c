7fbe13c9b2d6913457b29898c340fd4f2a52f9a9
Anton MIRONENKO <amironen@e1z2r4p41.42.us.org>
2017-06-02 13:05:13 -0700

    rekt

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if ((int)n < 1)
+		return (0);
 	while (n--)
 		if (*s1 != *s2)
 			return ((unsigned char)*s1 - (unsigned char)*s2);
